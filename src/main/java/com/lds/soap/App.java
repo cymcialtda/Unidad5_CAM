b19614b6edf235a4583d1b271eba96150179ea51
cesar.montoya <cesar.montoya@DESARROLLO-DP.LINEADATASCAN.local>
2017-12-18 19:20:40 -0500

    xxxxx

--- src/main/java/com/lds/soap/App.java
+++ src/main/java/com/lds/soap/App.java
@@ -1,30 +1,9 @@
 package com.lds.soap;
 
-import java.rmi.RemoteException;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
-import com.lds.soap.GlobalWeatherStub.GetCitiesByCountry;
-import com.lds.soap.GlobalWeatherStub.GetCitiesByCountryResponse;
-import com.lds.soap.GlobalWeatherStub.GetWeather;
-import com.lds.soap.GlobalWeatherStub.GetWeatherResponse;
 
 @ApplicationPath("/rest")
 public class App extends Application {
 
-//	public static void main(String[] args) throws RemoteException {
-//
-//		GlobalWeatherStub globalWeatherStub = new GlobalWeatherStub();
-//		GetCitiesByCountry getCitiesByCountry = new GetCitiesByCountry();
-//		getCitiesByCountry.setCountryName("Colombia");
-//		GetCitiesByCountryResponse getCitiesByCountryResponse = globalWeatherStub.getCitiesByCountry(getCitiesByCountry);
-//		System.out.println(getCitiesByCountryResponse.getGetCitiesByCountryResult());
-//
-//		GetWeather getWeather = new GetWeather();
-//		getWeather.setCityName("Medellin");
-//		getWeather.setCountryName("Colombia");
-//		
-//		GetWeatherResponse getWeatherResponse = globalWeatherStub.getWeather(getWeather);
-//		System.out.println(getWeatherResponse.getGetWeatherResult());
-//	}
 }
--- src/main/java/com/lds/soap/Endpoint.java
+++ src/main/java/com/lds/soap/Endpoint.java
@@ -9,32 +9,19 @@ import javax.ws.rs.Produces;
 
 import com.lds.soap.GlobalWeatherStub.GetCitiesByCountry;
 import com.lds.soap.GlobalWeatherStub.GetCitiesByCountryResponse;
-import com.lds.soap.GlobalWeatherStub.GetWeather;
-import com.lds.soap.GlobalWeatherStub.GetWeatherResponse;
 
 @Path("servlet")
 public class Endpoint {
 	@GET
-	@Path("/ciudad/{ciudad}")
+	@Path("/pais/{pais}")
 	@Produces("text/plain")
-	public String servlet3(@PathParam("ciudad") String identificador) throws RemoteException {
+	public String servlet3(@PathParam("pais") String identificador) throws RemoteException {
 		
 		GlobalWeatherStub globalWeatherStub = new GlobalWeatherStub();
 		GetCitiesByCountry getCitiesByCountry = new GetCitiesByCountry();
 		getCitiesByCountry.setCountryName(identificador);
 		GetCitiesByCountryResponse getCitiesByCountryResponse = globalWeatherStub.getCitiesByCountry(getCitiesByCountry);
-		//System.out.println(getCitiesByCountryResponse.getGetCitiesByCountryResult());
 
 		return getCitiesByCountryResponse.getGetCitiesByCountryResult();
-		
-		/*GetWeather getWeather = new GetWeather();
-		getWeather.setCityName("Medellin");
-		getWeather.setCountryName("Colombia");
-		
-		GetWeatherResponse getWeatherResponse = globalWeatherStub.getWeather(getWeather);
-		System.out.println(getWeatherResponse.getGetWeatherResult());
-		
-		
-		return "hola servlet 3 + " + identificador;*/
 	}
 }
